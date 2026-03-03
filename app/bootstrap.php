907b50b953f6d8bbcec8be1f82c8fd9330c5bfc4
josehenn7 <aldr4x.joseaugusto@gmail.com>
2026-03-03 19:53:30 -0300

    test

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = new Slim\App();
 
 $app->get('/', function ($request, $response) {
     return 'Hello, World!';
